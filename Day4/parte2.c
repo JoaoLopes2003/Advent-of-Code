badc6276673d13db9cbf0cb22faeb0b8533c2127
João Lopes <116074888+JoaoLopes2003@users.noreply.github.com>
2022-12-04 11:25:33 +0000

    Add files via upload

---
+++ Day4/parte2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int fullyCont = 0;
+    char line[100];
+
+    FILE *fp = fopen ("input.txt","r");
+
+    while (fgets(line,100,fp)!=NULL)
+    {
+        int min1, max1, min2, max2;
+        char *token;
+
+        token = strtok (line, "-");
+        min1 = atoi (token);
+
+        token = strtok (NULL, ",");
+        max1 = atoi (token);
+
+        token = strtok (NULL, "-");
+        min2 = atoi (token);
+
+        token = strtok (NULL, "-");
+        max2 = atoi (token);
+
+        if (!((min1<min2 && max1<min2) || (min2<min1 && max2<min1))) fullyCont++;
+    }
+
+    printf ("%d\n", fullyCont);
+
+    return 0;
+}
